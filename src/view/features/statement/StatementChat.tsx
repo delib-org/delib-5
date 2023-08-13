6c37a5d8a767af226de2f285070ecc0e2b398a18
Tal Yaron <tal.yaron@gmail.com>
2023-08-13 08:21:36 +0300

    can show more

--- src/view/features/statement/StatementChat.tsx
+++ src/view/features/statement/StatementChat.tsx
@@ -1,13 +1,15 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Statement } from '../../../model/statements/statementModel'
 import { auth } from '../../../functions/db/auth'
 
 interface Props {
   statement: Statement
-  showImage:Function
+  showImage: Function
 }
 
-const StatementChat: FC<Props> = ({ statement, showImage}) => {
+const StatementChat: FC<Props> = ({ statement, showImage }) => {
+
+  const [show, setShow] = useState(false)
 
   const userId = auth.currentUser?.uid;
   const userProfile = statement.creator.photoURL;
@@ -15,13 +17,20 @@ const StatementChat: FC<Props> = ({ statement, showImage}) => {
 
   const isMe = userId === creatorId;
 
+
+
   return (
     <>
 
       <div className={isMe ? `statement__chatCard statement__chatCard--me` : "statement__chatCard statement__chatCard--other"}>
-        <div onClick={()=>showImage(statement.creator)} className="statement__chatCard__profile" style={userProfile ? { backgroundImage: `url(${userProfile})` } : {}}></div>
+        <div onClick={() => showImage(statement.creator)} className="statement__chatCard__profile" style={userProfile ? { backgroundImage: `url(${userProfile})` } : {}}></div>
         <div className={isMe ? "bubble right" : "bubble left"}>
-          {statement.statement}
+          <div className="statement__bubble" onClick={() => setShow(!show)}>
+            {statement.statement}
+          </div>
+          {show ? <div className="statement__more">
+            test
+          </div> : null}
         </div>
       </div>
     </>
