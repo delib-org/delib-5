ec73ee0ce8dda073b483b2ea05182e31e506d1aa
Tal Yaron <tal.yaron@gmail.com>
2023-08-14 11:12:37 +0300

    we can now get consensus

--- functions/src/fn_evaluation.ts
+++ functions/src/fn_evaluation.ts
@@ -1,81 +1,156 @@
 import { logger } from "firebase-functions/v1";
 import { db } from "./index";
-import { runTransaction } from "firebase/firestore";
-import { DocumentSnapshot } from "firebase-admin/firestore";
+
 
 export async function updateEvaluation(event: any) {
     try {
-        logger.info("updateEvaluation");
-        const evaluationId = event.params.evaluationId;
-        logger.info("evaluationId", evaluationId);
-        const evaluation = event.data.after.data();
 
-        const userEvaluation = evaluation.evaluation;
-        const parentId = evaluation.parentId;
-        
-        if (!userEvaluation)
-            throw new Error("userEvaluation is not defined");
+        // const evaluationId = event.params.evaluationId;
 
+        const dataAfter = event.data.after.data();
+        const evaluation = dataAfter.evaluation;
+        if (evaluation === undefined) throw new Error("evaluation is not defined");
+
+        const dataBefore = event.data.before.data();
+        let previousEvaluation = 0;
+        if (dataBefore) previousEvaluation = dataBefore.evaluation || 0;
+
+        const evaluationDeferneces = evaluation - previousEvaluation;
+
+        const statementId = dataAfter.statementId;
+        if (!statementId) throw new Error("statementId is not defined");
+        const statementRef = db.collection("statements").doc(statementId);
+
+        const parentId = dataAfter.parentId;
         if (!parentId)
             throw new Error("parentId is not defined");
 
 
         //get parent statement
         const parentRef = db.collection("statements").doc(parentId);
-        const parentStatementDB = await parentRef.get();
-        logger.info("parentStatementDB", parentStatementDB.data());
-        let totalEvaluators = parentStatementDB.data().totalEvaluators || 0;
-        logger.info("totalEvaluators 1:", totalEvaluators);
 
-        //get the evluation this user evaluated inside the parent statement
-        const parentEvaluationsRef = db.collection("evaluations");
-        //look for an evaluation by the user in the parent statement
-        const q = parentEvaluationsRef.where("parentId", "==", parentId).where("evaluatorId", "==", evaluation.evaluatorId);
-        const parentEvaluationsDB = await q.get();
+        const statementEvaluatorsRef = db.collection("statementEvaluators");
+        const statementEvaluatorDB = await statementEvaluatorsRef.doc(`${dataAfter.evaluatorId}--${parentId}`).get();
 
+        const totalEvaluators = await updateNumberOfEvaluators(statementEvaluatorDB, statementEvaluatorsRef, dataAfter, parentId, parentRef);
+        logger.log("totalEvaluators", totalEvaluators);
+        const _totalEvaluations = await setNewEvaluation(statementRef, evaluationDeferneces);
 
-        let totalEvaluatorEvaluations = parentEvaluationsDB.size || 1;
-        logger.info("totalEvaluatorEvaluations", totalEvaluatorEvaluations);
+        const consensus = await calculateConsensus(_totalEvaluations, totalEvaluators);
 
-        if (totalEvaluatorEvaluations <= 1) {
-            logger.info("first time evaluating in this parent statment");
-            //if size of parentEvaluationsDB by evaluator is 0 then this is the first evaluation by this user on this statement.add it to the parent statement
-            await db.runTransaction(async (t: any) => {
-                try {
-
-                    const parentStatementDB = await t.get(parentRef);
-
-                    
-
-                    if (!parentStatementDB.exists) {
-                        throw new Error("parentStatementRef does not exist");
-                    }
-
-                    const newTotalEvaluators = parentStatementDB.data().totalEvaluators + 1 || 1;
-                    t.update(parentRef, { totalEvaluators: newTotalEvaluators });
-
-                    totalEvaluators = newTotalEvaluators;
-
-                } catch (error) {
-                    logger.error(error);
-                }
-            });
-        }
-
-        logger.info("totalEvaluators", totalEvaluators);
+        //set consensus to statement in DB
+        await statementRef.update({ consensus });
 
 
-        //calculate over all evaluation
-        //how do we evluate consensus?
-        // total particpents in a statement
-        // number of evaluators on a sub statement
-        //log2 abs((positive_evaluations - negative_evaluations)) / total_evaluators_in_parent_statement
 
-        //(pst-neg, 1, -1)(log1.3(abs(atLeast 1(positive_evaluators_evaluation - negative_evaluators_evaluation)))) / total_evaluators
+
+
 
 
     } catch (error) {
         logger.error(error);
         return;
     }
+
+    async function calculateConsensus(_totalEvaluations: number, totalEvaluatorEvaluations: any) {
+        //(pst-neg, 1, -1)(log1.3(abs(atLeast 1(positive_evaluators_evaluation - negative_evaluators_evaluation)))) / total_evaluators
+        try {
+
+
+            const proevaluation = (() => {
+                if (_totalEvaluations < 0) {
+                    return -1;
+                }
+                return 1;
+            })();
+
+            const totalEvaluations = beforeLogCalculation(Math.abs(_totalEvaluations));
+            const totalLogEvaluations = getBaseLog(totalEvaluations, 1.3);
+            const consensus = proevaluation * (totalLogEvaluations / totalEvaluatorEvaluations);
+
+            return consensus;
+        } catch (error) {
+            logger.error(error);
+            return { consensus: 0, _totalEvaluations: 0 };
+        }
+    }
+
+    async function setNewEvaluation(statementRef: any, evaluationDeferneces: number): Promise<number> {
+        let newTotalEvaluations: number = 0;
+        await db.runTransaction(async (t: any) => {
+            try {
+                const statementDB = await t.get(statementRef);
+
+                if (!statementDB.exists) {
+                    throw new Error("statement does not exist");
+                }
+
+                newTotalEvaluations = statementDB.data().totalEvaluations;
+                if (newTotalEvaluations === undefined)
+                    newTotalEvaluations = 0;
+
+
+                newTotalEvaluations += evaluationDeferneces;
+
+                t.update(statementRef, { totalEvaluations: newTotalEvaluations });
+
+                // return newTotalEvaluations
+            } catch (error) {
+                logger.error(error);
+                // return 0;
+            }
+        });
+        return newTotalEvaluations;
+    }
+
+    async function updateNumberOfEvaluators(statementEvaluatorDB: any, statementEvaluatorsRef: any, dataAfter: any, parentId: any, parentRef: any) {
+        try {
+            let totalEvaluators = 0;
+            if (!statementEvaluatorDB.exists) {
+                //add to statementEvaluators
+                await statementEvaluatorsRef.doc(`${dataAfter.evaluatorId}--${parentId}`).set({ evaluatorId: dataAfter.evaluatorId, parentId: parentId });
+
+                //if size of parentEvaluationsDB by evaluator is 0 then this is the first evaluation by this user on this statement.add it to the parent statement
+                await db.runTransaction(async (t: any) => {
+                    try {
+
+                        const parentStatementDB = await t.get(parentRef);
+
+
+
+                        if (!parentStatementDB.exists) {
+                            throw new Error("parentStatementRef does not exist");
+                        }
+
+                        const newTotalEvaluators = parentStatementDB.data().totalEvaluators + 1 || 1;
+                        t.update(parentRef, { totalEvaluators: newTotalEvaluators });
+
+                        totalEvaluators = newTotalEvaluators;
+                    } catch (error) {
+                        logger.error(error);
+                    }
+                });
+            } else{
+                const parentStatementDB = await parentRef.get();
+                if (!parentStatementDB.exists) {
+                    throw new Error("parentStatementRef does not exist");
+                }   
+                totalEvaluators = parentStatementDB.data().totalEvaluators ||0;
+            }
+            return totalEvaluators;
+        } catch (error) {
+            logger.error(error);
+            return 0;
+        }
+    }
+    function getBaseLog(x: number, baseLog: number): number {
+        return Math.log(baseLog) / Math.log(x);
+    }
+}
+
+function beforeLogCalculation(totalEvaluations: number) {
+    if (totalEvaluations < 1) {
+        return 1
+    }
+    return totalEvaluations
 }
