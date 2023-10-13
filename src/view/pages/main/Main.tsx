d1d06cf4f9c6eed66d69ba8b194841bb7d88ef19
Tal <tal.yaron@gmail.com>
2023-10-13 12:49:31 +0300

    preinstall

--- src/view/pages/main/Main.tsx
+++ src/view/pages/main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Fav from '../../components/fav/Fav';
 
@@ -19,9 +19,34 @@ const Main = () => {
     const isLgged = useAuth();
     const dispatch = useAppDispatch();
 
+    const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+
     function updateStoreStSubCB(statementSubscription: StatementSubscription) {
         dispatch(setStatementSubscription(statementSubscription));
     }
+
+    useEffect(() => {
+
+        //@ts-ignore
+        if (window.navigator && (window.navigator.standalone || window.navigator.userAgentData || window.navigator.getInstalledRelatedApps)) {
+            alert("The web app is installed as a PWA.");
+        } else {
+            alert("The web app is not installed as a PWA.");
+        }
+
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+
+            e.preventDefault();
+
+            console.log('beforeinstallprompt fired');
+           
+
+            setDeferredPrompt(e);
+            // Update UI notify the user they can install the PWA
+            // showInstallPromotion();
+        });
+    }, [])
 
     useEffect(() => {
 
@@ -36,6 +61,23 @@ const Main = () => {
 
 
 
+    function handleInstallApp() {
+        try {
+            console.log('handleInstallApp')
+            if (deferredPrompt) {
+                deferredPrompt.prompt();
+                deferredPrompt.userChoice.then((choiceResult: any) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    }
+                    setDeferredPrompt(null);
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     function handleAddStatment() {
         navigate('/home/addStatment')
     }
@@ -49,7 +91,10 @@ const Main = () => {
             <div className="page__header">
                 <h1>דליב</h1>
                 <h2> יוצרים הסכמות </h2>
-                <button onClick={handleLogout}>Logout</button>
+                <div className="btns">
+                    <button onClick={handleLogout}>התנתקות</button>
+                    <button onClick={handleInstallApp}>התקנת האפליקציה</button>
+                </div>
             </div>
             <div className="page__main">
                 <div className="wrapper">
