5a3a18d4c3dad7b68616fd7e2356f656a2a22126
Tal <tal.yaron@gmail.com>
2023-10-15 11:33:45 +0300

    cont

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fet2adrp0k8"
+    "revision": "0.m8m3i5h2f38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/view/components/accessibility/Accessiblity.tsx
+++ src/view/components/accessibility/Accessiblity.tsx
@@ -1,35 +1,54 @@
 import AccessibleIcon from '@mui/icons-material/Accessible';
 import { useAppDispatch, useAppSelector } from '../../../functions/hooks/reduxHooks';
 import { fontSizeSelector, increaseFontSize } from '../../../model/accessibility/accessibiliySlice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Accessiblity = () => {
     const dispatch = useAppDispatch();
-    const fontSize = useAppSelector(fontSizeSelector)
+    const fontSize = useAppSelector(fontSizeSelector);
+    const [isOpen, setIsOpen] = useState(false);
 
-function handleChangeFontSize(number:number) {
-    dispatch(increaseFontSize(number))
+    useEffect(() => {
+        document.documentElement.style.fontSize = fontSize + 'px';
+        document.body.style.fontSize = fontSize + 'px';
+    }, [fontSize])
 
-}
+    function handleChangeFontSize(number: number) {
+        dispatch(increaseFontSize(number))
 
-useEffect(() => {
-    document.documentElement.style.fontSize = fontSize + 'px';
-    document.body.style.fontSize = fontSize + 'px';
-}, [fontSize])
+    }
+
+    function handleOpen() {
+        if (isOpen) {
+            // If it's not open, open it and start the timer to close it after 2 seconds
+            setIsOpen(false);
+    
+            
+        } else {
+            // If it's already open, close it immediately
+            setIsOpen(true);
+            setTimeout(() => {
+                setIsOpen(false);
+                console.log("close")
+            }, 14000); // 7000 milliseconds (2 seconds)
+        }
+    }
+
+
 
     return (
-        <div className="accessibility">
-            <div className='accessibility__button'>
+        <div className="accessibility" style={!isOpen ? { left: "-12.6rem" } : { left: "0rem" }}>
+            <div className='accessibility__button' onClick={handleOpen}>
                 <AccessibleIcon htmlColor='white' />
             </div>
             <div className="accessibility__fonts">
-                
-                <div className="accessibility__fonts__control" onClick={()=>handleChangeFontSize(1)}>+</div>
+
+                <div className="accessibility__fonts__control" onClick={() => handleChangeFontSize(1)}>+</div>
                 <div className="accessibility__fonts__size">{fontSize}px</div>
-                <div className="accessibility__fonts__control" onClick={()=>handleChangeFontSize(-1)}>-</div>
+                <div className="accessibility__fonts__control" onClick={() => handleChangeFontSize(-1)}>-</div>
                 <span dir="ltr">Fonts:</span>
             </div>
-            
+
         </div>
     )
 }
