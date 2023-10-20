5963a328b91882a55c491114e232d5967a6fa544
Tal <tal.yaron@gmail.com>
2023-10-20 08:49:20 +0300

    participant can see the room she is invited to

--- src/view/pages/statement/components/rooms/RoomDivide.tsx
+++ src/view/pages/statement/components/rooms/RoomDivide.tsx
@@ -1,7 +1,7 @@
 import { Statement } from 'delib-npm';
 import { FC } from 'react';
-import {  useAppSelector } from '../../../../../functions/hooks/reduxHooks';
-import {  userSelectedRoomSelector, userSelectedTopicSelector } from '../../../../../model/statements/statementsSlice';
+import { useAppSelector } from '../../../../../functions/hooks/reduxHooks';
+import { userSelectedRoomSelector, userSelectedTopicSelector } from '../../../../../model/statements/statementsSlice';
 import { auth } from '../../../../../functions/db/auth';
 import AdminSeeAllGroups from './admin/AdminSeeAllGroups';
 
@@ -15,24 +15,28 @@ interface Props {
 const RoomQuestions: FC<Props> = ({ statement }) => {
 
   const userTopic = useAppSelector(userSelectedTopicSelector(statement.statementId));
-const userRoom = useAppSelector(userSelectedRoomSelector(statement.statementId));
-console.log(statement.statementId);
-console.log(auth.currentUser?.uid)
-console.log(userRoom)
+  const userRoom = useAppSelector(userSelectedRoomSelector(statement.statementId));
+  console.log(statement.statementId);
+  console.log(auth.currentUser?.uid)
+  console.log(userRoom)
 
   const isAdmin = statement.creatorId === auth.currentUser?.uid;
 
   try {
 
-    
+
 
 
 
     return (
       <>
         <h1>חלוקה לחדרים</h1>
-        {userTopic ? <h1>נושא: {userTopic.statement.statement}</h1> : null}
-       {isAdmin?<AdminSeeAllGroups statement={statement} />:null} 
+        {userTopic ?
+          <div>
+            <h1>נושא: {userTopic.statement.statement}</h1>
+            <h2>מוזמן לחדר מספר: {userTopic.roomNumber}</h2>
+          </div> : null}
+        {isAdmin ? <AdminSeeAllGroups statement={statement} /> : null}
       </>
     )
   } catch (error: any) {
