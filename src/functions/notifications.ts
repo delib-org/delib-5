fdacfa0613d53f090689c77038411c6488b1e9f4
Tal <tal.yaron@gmail.com>
2023-10-24 12:00:09 +0300

    improved UX for notifications

--- src/functions/notifications.ts
+++ src/functions/notifications.ts
@@ -3,14 +3,12 @@ export async function getUserPermissionToNotifications(): Promise<boolean> {
        
         if (!("Notification" in window)) throw new Error("Notification not supported");
         if (Notification.permission === "granted") return true;
-        if (Notification.permission === "denied") {
-            console.log('permission denied')
-            const permission = await Notification.requestPermission();
-            if (permission !== "granted") throw new Error("Permission not granted");
-            return true;
-        }
-
+        if (Notification.permission === "denied") throw new Error("Permission not granted");
+       
+        //in case the user didn't set the notification permission yet
+        alert("אנא אשר/י התראות, כדי לקבל עדכונים על תגובות חדשות\nתוכל/י לבטל את ההתראות בכל זמן");
         const permission = await Notification.requestPermission();
+        
         if (permission !== "granted") throw new Error("Permission not granted");
         return true;
     } catch (error) {
